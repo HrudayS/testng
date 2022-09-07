10eaf3cc1151aa22611e741b7b036c1aa484c928
HRUDAY <HRUDAY@DESKTOP-EDU5TT3>
2022-09-07 11:07:13 +0530

    commit:commited

---
+++ src/testng_demo/class1.java
@@ -0,0 +1,15 @@
+package testng_demo;
+
+public class class1 {
+	
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is testcase1");
+		
+		
+
+	}
+
+}
---
+++ src/testng_demo/class2.java
@@ -0,0 +1,13 @@
+package testng_demo;
+
+import org.testng.annotations.Test;
+
+public class class2 {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("this is testcase2"); 
+	}
+
+}
